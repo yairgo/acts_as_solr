eb975028e90979f45e216df54acd5f217f929b03
Mathias Meyer <meyer@paperplanes.de>
2009-04-19 21:57:24 +0200

    Added new test bodies for the new classes.

--- test/unit/test_helper.rb
+++ test/unit/test_helper.rb
@@ -4,6 +4,7 @@ $:.unshift(File.join(File.expand_path(File.dirname(__FILE__))))
 require 'rubygems'
 require 'test/unit'
 require 'activesupport'
+require 'active_support/test_case'
 require 'test_base'
 
 require 'mocha'
